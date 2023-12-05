cdb534e7b74daf247898487a955fedd3600c2511
RaihanSultana <raihansultana1418@gmail.com>
2023-12-04 17:16:26 -0700

    login page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Login from './Login'
 
 
 
-function App() {
+function App(){
   return (
     <Router>
       <div className='App'>
-        <Header />
+        <Routes>
+          <Route path='/' element={<Header/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
 export default App;
